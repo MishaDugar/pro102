85f24dca2aca92fe451b8d88013d02a732692cad
MishaDugar <71166822+MishaDugar@users.noreply.github.com>
2022-02-01 13:29:57 +0530

    Add files via upload

---
+++ classpro102.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+import time
+
+targettime=time.localtime(time.time())
+def main (): 
+    while(True):
+        if(targettime.tm_hour=="12"):
+            days=30
+            path="F:/Documents/Zoom"
+            seconds=time.time()-days*24*60*60
+            if os.path.exists(path):
+                for rootfolder,folders,files in os.walk(path):
+                    if seconds>= getfileorfolderage(rootfolder):
+                        removefolder(rootfolder)
+                        break
+                    else:
+                        for folder in folders:
+                            folderpath=os.path.join(rootfolder,folder)
+                            if seconds>= getfileorfolderage(folderpath):
+                                removefolder(folderpath)
+
+                        for file in files:
+                            filepath=os.path.join(rootfolder,file)
+                            if seconds>= getfileorfolderage(filepath):
+                                removefile(filepath)
+            else:
+                print("path not found")
+
+def removefolder(path):
+    shutil.rmtree(path)
+    print("path deleted sucessfully")
+
+def removefile(path):
+    os.remove(path)
+    print("file removed sucessfully")
+
+def getfileorfolderage(path):
+    ctime=os.stat(path).st_ctime
+    return ctime
+
+main()
+                        
+             
